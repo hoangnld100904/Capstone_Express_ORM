d5e375cdf8913249f4679d158e9aa400b0ceef4b
Hoang <nldhoang10092004@gmail.com>
2023-05-20 21:37:59 +0700

    a

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,20 @@
 const { errorCode, failCode, successCode } = require("../ultis/respondse");
+const { generateToken, checkToken, decodeToken } = require("../ultis/jwt");
 //Import prisma
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient(); //tương tự initModel của sequelize
-
+//Import Bcrypt
+const bcryptjs = require("bcryptjs");
 //Function
 const createUser = async (req, res) => {
   try {
     const { email, password, fullName, age } = req.body;
-    let newData = { email, mat_khau: password, ho_ten: fullName, tuoi: age };
+    let newData = {
+      email,
+      mat_khau: bcryptjs.hashSync(password, 10),
+      ho_ten: fullName,
+      tuoi: age,
+    };
     await prisma.nguoi_dung.create({ data: newData });
     successCode(res, "Tạo user thành công", newData, 201);
   } catch (err) {
@@ -48,21 +55,74 @@ const deleteUser = async (req, res) => {
       },
     });
     if (checkUser) {
-        await prisma.nguoi_dung.delete({
-            where: {
-                nguoi_dung_id: Number(id)
-            }
-        })
-        successCode(res, "Xoá user thành công", "", 200)
-    } else{
-        failCode(res, "Id người dùng không tồn tại", id, 404)
+      await prisma.nguoi_dung.delete({
+        where: {
+          nguoi_dung_id: Number(id),
+        },
+      });
+      successCode(res, "Xoá user thành công", "", 200);
+    } else {
+      failCode(res, "Id người dùng không tồn tại", id, 404);
     }
-  } catch (err){
-    errorCode(res, err)
+  } catch (err) {
+    errorCode(res, err);
+  }
+};
+const registerUser = async (req, res) => {
+  try {
+    const { email, password, fullName, age } = req.body;
+    let checkUser = prisma.nguoi_dung.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (!checkUser){
+      let newData = {
+        email,
+        mat_khau: bcryptjs.hashSync(password, 10),
+        ho_ten: fullName,
+        tuoi: age,
+      };
+      await prisma.nguoi_dung.create({ data: newData });
+      successCode(res, "Đăng ký thành công", newData, 201);
+    } else{
+      failCode(res, "Email đã được sử dụng", email, 400)
+    }
+  } catch (err) {
+    errorCode(res, err);
+  }
+};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let checkUser = prisma.nguoi_dung.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (checkUser) {
+      let checkPassword = bcryptjs.compareSync(password, checkUser.password)
+      if (checkPassword) {
+        let userData = {
+          email,
+          role:"user"
+        }
+        let token = generateToken(userData)
+        successCode(res, "Login thành công", token, 200)
+      } else{
+        failCode(res, "Mật khẩu không đúng", "", 400)
+      }
+    } else{
+      failCode(res, "Email chưa được đăng ký tài khoản", "", 404)
+    }
+  } catch (err) {
+    errorCode(res, err);
   }
 };
 module.exports = {
-    createUser,
-    updateUser,
-    deleteUser
-}
+  createUser,
+  updateUser,
+  deleteUser,
+  registerUser,
+  loginUser
+};
--- src/routers/userRoute.js
+++ src/routers/userRoute.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { createUser, updateUser, deleteUser } = require("../controllers/userController");
+const {
+  createUser,
+  updateUser,
+  deleteUser,
+  registerUser,
+  loginUser,
+} = require("../controllers/userController");
+const { checkToken, privateAPI } = require("../ultis/jwt");
 
 const userRouter = express.Router();
 
-userRouter.post("/create-user", createUser )
-userRouter.put("/update-user/:id", updateUser )
-userRouter.delete("/delete-user/:id", deleteUser)
-module.exports = userRouter
+userRouter.post("/create-user", createUser);
+userRouter.put("/update-user/:id", updateUser);
+userRouter.delete("/delete-user/:id", deleteUser);
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser)
+module.exports = userRouter;
